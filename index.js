d633f24c38e25de58fa9a343ef8b304952fd0fc2
Dallas Hepner <dallashepner@gmail.com>
2022-07-20 17:13:38 -0600

    Audio Button

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let button = document.querySelector('button')
+let audio = new Audio("http://youtuberepeater.com/watch?v=Yael6mbNyfo&name=you+39+re+trash+kid+sound+effect#gsc.tab=0")
 
 const buttonAlert = () => {
+    audio.play();
     alert(`Get good kid!`)
 }
 
