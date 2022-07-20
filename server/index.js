26c0433128fbe449865db44713797ee604ade984
Dallas Hepner <dallashepner@gmail.com>
2022-07-20 15:06:48 -0600

    Added text

--- server/index.js
+++ server/index.js
@@ -6,6 +6,12 @@ const app = express()
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../styles.css'))
+})
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../main.js'))
+})
 
 const port = process.env.PORT || 4444
 
