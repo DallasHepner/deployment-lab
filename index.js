5d5e0c52134308a0b289a976d13e8413e9c73af1
Dallas Hepner <dallashepner@gmail.com>
2022-07-21 14:14:01 -0600

    Removed rollbar from index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ let button = document.querySelector('button')
 let audio = new Audio("trash_Kid.mp3")
 
 const buttonAlert = () => {
-    rollbar.info("Someone clicked your button!")
     audio.play();
     alert(`Get good kid!`)
 }
