c9c7bc5af32c426689114fd4b09eb6d482b48898
Dallas Hepner <dallashepner@gmail.com>
2022-07-21 14:11:40 -0600

    Added rollbar

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let button = document.querySelector('button')
 let audio = new Audio("trash_Kid.mp3")
 
 const buttonAlert = () => {
+    rollbar.info("Someone clicked your button!")
     audio.play();
     alert(`Get good kid!`)
 }
--- server/index.js
+++ server/index.js
@@ -1,15 +1,33 @@
 const express = require('express')
 const path = require('path')
-
+const cors = require('cors')
 const app = express()
 
+app.use(express.json())
+app.use(cors())
+
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '194549d3a7ec4b7db4291391bc51e8da',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+
 app.get('/', (req, res) => {
+    rollbar.log("Someone loaded up your html!")
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 app.get('/css', (req, res) => {
+    rollbar.log("Someone loaded your css")
     res.sendFile(path.join(__dirname, '../index.css'))
 })
 app.get('/js', (req, res) => {
+    rollbar.log("Someone loaded your js!")
     res.sendFile(path.join(__dirname, '../index.js'))
 })
 
